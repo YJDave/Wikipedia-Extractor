ea6a6eefd152bb27518601cd626662f644945f60
Yashashvi Dave <yashashvidave@gmail.com>
2019-04-30 23:46:31 +0530

    Extract function to scrap subsection data.

--- app.py
+++ app.py
@@ -34,35 +34,42 @@ def clean_str(x):
     return clean_str
 
 
-# Extract HTML content from given url
-html = urllib.request.urlopen(url)
-# Beautify the HTML content
-soup = BeautifulSoup(html, features="html.parser")
+def extract_subsection_content(url, sub_section, text_file_name):
+    # Extract HTML content from given url
+    html = urllib.request.urlopen(url)
+    # Beautify the HTML content
+    soup = BeautifulSoup(html, features="html.parser")
 
-title = soup.find("h1", {"id": "firstHeading"})
+    title = soup.find("h1", {"id": "firstHeading"})
 
-# Will take the Machine Learning URL 
-p = wikipedia.page(title.text)
+    # Will take the Machine Learning URL 
+    p = wikipedia.page(title.text)
 
-# Will find the subsection from the given article
-subsection_content = p.section(sub_section)
+    # Will find the subsection from the given article
+    subsection_content = p.section(sub_section)
 
-# print(subsection_content)
-file = open(text_file_name, "w")
-file.write(subsection_content)
-file.close()
+    # print(subsection_content)
+    file = open(text_file_name, "w")
+    file.write(subsection_content)
+    file.close()
 
-sc = SparkContext("local", "WikipediaExtractor App")
+    sc = SparkContext("local", "WikipediaExtractor App")
 
-text_file_content = sc.textFile(text_file_name)
+    text_file_content = sc.textFile(text_file_name)
 
-### Read the content of file and count words from it below:
+    ### Read the content of file and count words from it below:
 
-non_empty_text = text_file_content.filter(lambda x: len(x) > 0)
-words = non_empty_text.flatMap(lambda x: clean_str(x).split(' '))
-word_count = words.map(lambda x:(x, 1)).reduceByKey(lambda x, y: x + y) \
-                  .map(lambda x: (x[1], x[0])) \
-                  .sortByKey(False)
+    non_empty_text = text_file_content.filter(lambda x: len(x) > 0)
+    words = non_empty_text.flatMap(lambda x: clean_str(x).split(' '))
+    word_count = words.map(lambda x:(x, 1)).reduceByKey(lambda x, y: x + y) \
+                      .map(lambda x: (x[1], x[0])) \
+                      .sortByKey(False)
 
-for word in word_count.collect():
-    print(word)
+    for word in word_count.collect():
+        print(word)
+
+    return word_count
+
+###
+
+extract_subsection_content(url, sub_section, text_file_name)
